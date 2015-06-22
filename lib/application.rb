1eff57c0f4ddae77e06c5e5b2abd28acc367059a
tohrinagi <tohrinagi@gmail.com>
2015-06-23 01:07:45 +0900

    リスト画面を整形、CSS追加

--- lib/application.rb
+++ lib/application.rb
@@ -3,10 +3,28 @@
 require 'sinatra/base'
 require 'haml'
 
+require 'todo/db'
+require 'todo/task'
+
 module Todo
   class Application < Sinatra::Base
+    configure do
+      DB.prepare
+    end
+
+    configure :development do
+      require 'sinatra/reloader'
+      register Sinatra::Reloader
+    end
 
     get '/' do
+      redirect '/tasks'
+    end
+
+
+    get '/tasks' do
+      @tasks = Task.all
+
       haml :index
     end
 
